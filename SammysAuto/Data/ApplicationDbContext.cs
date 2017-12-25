0d100030b59db35abd28abe8bc763b9f0221a499
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 10:33:16 -0600

    Add Service Type Model

--- SammysAuto/Data/ApplicationDbContext.cs
+++ SammysAuto/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace SammysAuto.Data
         {
         }
 
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
--- SammysAuto/Data/Migrations/20171225163103_AddServiceTypeToDB.Designer.cs
+++ SammysAuto/Data/Migrations/20171225163103_AddServiceTypeToDB.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SammysAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171225163103_AddServiceTypeToDB")]
+    partial class AddServiceTypeToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
