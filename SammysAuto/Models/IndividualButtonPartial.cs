13a7073a68f5e08f1e41af13bb001587c8c2def5
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 11:36:06 -0600

    Individual Button Partial

---
+++ SammysAuto/Models/IndividualButtonPartial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SammysAuto.Models
+{
+    public class IndividualButtonPartial
+    {
+        public string ButtonType { get; set; }
+        public string Action { get; set; }
+        public string Glyph { get; set; }
+        public string Text { get; set; }
+
+        public int? ServiceId { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                var param = new StringBuilder(@"/");
+                if (ServiceId != 0 && ServiceId != null)
+                {
+                    param.Append(String.Format("{0}", ServiceId));
+                }
+
+                return param.ToString().Substring(0, param.Length);
+            }
+        }
+    }
+}
