09bfcacc70c1e0faa71ec093371e9d22dc17d0b7
Ben Spark <bhrugen1990@gmail.com>
2017-12-26 11:51:41 -0600

    Services- View Model

---
+++ SammysAuto/ViewModel/CarAndServicesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SammysAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.ViewModel
+{
+    public class CarAndServicesViewModel
+    {
+        public Car CarObj { get; set; }
+        public Service NewServiceObj { get; set; }
+        public IEnumerable<Service> PastServicesObj { get; set; }
+        public List<ServiceType> ServiceTypesObj { get; set; }
+    }
+}
