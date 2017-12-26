199e88d4621f54c222347ccbc6304ef9a5ad10f4
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 21:11:57 -0600

    Users  - Create User View Model

---
+++ SammysAuto/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.ViewModels
+{
+    public class UserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Id { get; set; }
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+}
