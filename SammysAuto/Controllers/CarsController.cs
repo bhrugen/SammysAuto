74f3423b47bc9365d716c64c653f36851ee00da7
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 23:39:44 -0600

    Cars- Create Index Action

---
+++ SammysAuto/Controllers/CarsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SammysAuto.Data;
+using System.Security.Claims;
+using SammysAuto.ViewModel;
+
+namespace SammysAuto.Controllers
+{
+    public class CarsController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CarsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index(string userId = null)
+        {
+            if (userId == null)
+            {
+                //Only called when a customer logs in
+                userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+
+            var model = new CarAndCustomerViewModel
+            {
+                Cars = _db.Cars.Where(c=>c.UserId == userId),
+                UserObj = _db.Users.FirstOrDefault(u=>u.Id==userId)
+            };
+
+            return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
