68330157410556cf2c83380eb691a6eeb46eaed4
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 21:31:30 -0600

    Users - Create Users Controller and Index Action

---
+++ SammysAuto/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SammysAuto.Data;
+
+namespace SammysAuto.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _db.Users.ToList();
+
+            return View(users);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
