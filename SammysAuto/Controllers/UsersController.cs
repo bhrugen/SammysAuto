b4d9dee26a6dfa9d493391bee63f43c8b7ecf0de
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 21:59:00 -0600

    Users - Search button in Index View

--- SammysAuto/Controllers/UsersController.cs
+++ SammysAuto/Controllers/UsersController.cs
@@ -17,10 +17,28 @@ namespace SammysAuto.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string option=null, string search=null)
         {
             var users = _db.Users.ToList();
-
+            if (option == "email" && search != null)
+            {
+                users = _db.Users.Where(u => u.Email.ToLower().Contains(search.ToLower())).ToList();
+            }
+            else
+            {
+                if (option == "name" && search != null)
+                {
+                    users = _db.Users.Where(u => u.FirstName.ToLower().Contains(search.ToLower())
+                            || u.LastName.ToLower().Contains(search.ToLower())).ToList();
+                }
+                else
+                {
+                    if (option == "phone" && search != null)
+                    {
+                        users = _db.Users.Where(u => u.PhoneNumber.ToLower().Contains(search.ToLower())).ToList();
+                    }
+                }
+            }
             return View(users);
         }
 
