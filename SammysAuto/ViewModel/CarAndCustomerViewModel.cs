cf75c0d02186d2f465d1a4e4a2bd420a28084ce6
Ben Spark <bhrugen1990@gmail.com>
2017-12-25 23:30:21 -0600

    Car - Add View Model

---
+++ SammysAuto/ViewModel/CarAndCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SammysAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.ViewModel
+{
+    public class CarAndCustomerViewModel
+    {
+        public ApplicationUser UserObj { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+    }
+}
